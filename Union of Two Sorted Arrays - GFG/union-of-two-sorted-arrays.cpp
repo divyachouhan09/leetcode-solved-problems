9f2f29c3206d650dd58ada7913b26394ca9d7eb8
Divya Chouhan <76164230+divyachouhan09@users.noreply.github.com>
2023-02-12 18:52:25 +0530

    Added solution - LeetHub

--- Union of Two Sorted Arrays - GFG/union-of-two-sorted-arrays.cpp
+++ Union of Two Sorted Arrays - GFG/union-of-two-sorted-arrays.cpp
@@ -6,28 +6,69 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    //arr1,arr2 : the arrays
-    // n, m: size of arrays
+    
     //Function to return a list containing the union of the two arrays. 
     vector<int> findUnion(int arr1[], int arr2[], int n, int m)
     {
-        vector <int> v;
-        int a[1000000]={0};
+        int i = 0, j = 0; 
+        vector<int> ans;
+        //Using two pointers i and j over the two arrays which helps
+        //in storing the smaller element.
+        while (i < n && j < m) 
+        { 
+            //Updating the pointer i until we have identical 
+            //elements at consecutive position in arr1.
+            while(  i<n  &&  arr1[i]==arr1[i+1]  )
+                i++;
+               
+            //Updating the pointer j until we have identical 
+            //elements at consecutive position in arr2.
+            while(  j<m  &&  arr2[j]==arr2[j+1]  )
+                j++;
+                
         
-        for(int i=0; i<n; i++)
+            //Comparing element of the arrays arr1 and arr2 at pointers
+            //i and j and accordingly storing the smaller element
+            //and updating the pointers.
+            if (arr1[i] < arr2[j]) 
+                ans.push_back(arr1[i++]);
+          
+            else if (arr2[j] < arr1[i]) 
+                ans.push_back(arr2[j++]);
+                
+            else
+            {
+                //If arr1[i] is same as arr2[j], we update both pointers.
+                ans.push_back(arr2[j++]);
+                i++;
+            }
+        } 
+        
+        //Storing the remaining elements of first array (if there are any).
+        while(i < n)
         {
-            a[arr1[i]]++;
+            //Updating the pointer i until we have identical 
+            //elements at consecutive position in arr1.
+            while(  i<n  &&  arr1[i]==arr1[i+1]  )
+                i++;
+            
+            //Storing the elements.
+            ans.push_back(arr1[i++]);
         }
-        for(int i=0; i<m; i++)
+        //Storing the remaining elements of second array (if there are any).
+        while(j < m)
         {
-            a[arr2[i]]++;
+            //Updating the pointer j until we have identical 
+            //elements at consecutive position in arr2.
+            while(  j<m  &&  arr2[j]==arr2[j+1]  )
+                j++;
+            
+             //Storing the elements
+            ans.push_back(arr2[j++]);
         }
-        for(int i=0; i<1e6; i++)
-        {
-            if(a[i]!=0)
-            v.push_back(i);
-        }
-        return v;
+        
+        //returning the list containing the union of the two arrays. 
+        return ans;
     }
 };
 
